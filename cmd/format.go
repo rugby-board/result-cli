25aabecc43c5a4db50e8f26477570d6164cd90a1
David Zhang <crispgm@gmail.com>
2018-06-11 16:03:16 +0800

    Output markdown table

---
+++ cmd/format.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/rugby-board/result-cli/match"
+)
+
+// OutputMarkdownTable print a formatted markdown table
+func OutputMarkdownTable(matches []*match.Match) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"主队", "比分", "客队"})
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+	for _, m := range matches {
+		score := fmt.Sprintf("%d:%d", m.Team1Score, m.Team2Score)
+		table.Append([]string{m.Team1Name, score, m.Team2Name})
+	}
+	table.Render()
+}
