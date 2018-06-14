481bafc5489a4ab23adaacb03e061a39eddfdb78
David Zhang <crispgm@gmail.com>
2018-06-14 15:10:05 +0800

    Support base url in config

--- retriever/retriever.go
+++ retriever/retriever.go
@@ -3,19 +3,22 @@ package retriever
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/rugby-board/result-cli/match"
-)
-
-const (
-	// BaseURL ...
-	BaseURL = "http://kratos.365.co.za:9001/getresultsbycompidanddaterange/%d/%s/%s"
+	yaml "gopkg.in/yaml.v2"
 )
 
 // Retriever struct
 type Retriever struct {
-	client *http.Client
+	client  *http.Client
+	baseURL string
+}
+
+type conf struct {
+	BaseURL string `yaml:"base_url"`
 }
 
 // NewRetriever returns a Retriever
@@ -26,13 +29,25 @@ func NewRetriever() *Retriever {
 // Init initialize HTTP client
 func (r *Retriever) Init() error {
 	r.client = &http.Client{}
+	yamlFile, err := ioutil.ReadFile("conf/conf.yaml")
+	if err != nil {
+		log.Printf("Read file failed: %#v", err)
+		return err
+	}
+	c := &conf{}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Printf("Unmarshal failed: %#v", err)
+		return err
+	}
+	r.baseURL = c.BaseURL
 	return nil
 }
 
 // Retrieve results
 func (r *Retriever) Retrieve(eventID int32, dateStart string, dateEnd string) ([]*match.Match, error) {
 	m := make([]*match.Match, 0)
-	url := fmt.Sprintf(BaseURL, eventID, dateStart, dateEnd)
+	url := fmt.Sprintf(r.baseURL, eventID, dateStart, dateEnd)
 	resp, err := r.client.Get(url)
 	if err != nil {
 		return m, err
