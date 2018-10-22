d740933cda291dcbde6a457105453af98d2b0410
David Zhang <crispgm@gmail.com>
2018-10-22 11:30:58 +0800

    Make column's width larger

--- cmd/format.go
+++ cmd/format.go
@@ -8,11 +8,14 @@ import (
 	"github.com/rugby-board/result-cli/match"
 )
 
+const maxColWidth = 120
+
 // OutputMarkdownTable print a formatted markdown table
 func OutputMarkdownTable(matches []*match.Match) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"主队", "比分", "客队"})
 	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetColWidth(maxColWidth)
 	table.SetCenterSeparator("|")
 	for _, m := range matches {
 		score := fmt.Sprintf("%d-%d", m.Team1Score, m.Team2Score)
