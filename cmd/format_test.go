cf1b691e1f1ee1931303b42b44422c2e54323387
David Zhang <crispgm@gmail.com>
2019-03-27 14:49:08 +0800

    Fix format_test bringing by OutputMarkdownTable

--- cmd/format_test.go
+++ cmd/format_test.go
@@ -1,34 +1,10 @@
 package cmd
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"os"
 	"testing"
 
 	"github.com/rugby-board/rugby-result/match"
 )
-
-// Reference: https://github.com/kami-zh/go-capturer/blob/master/main.go
-func captureOutput(f func()) string {
-	r, w, err := os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-	stdout := os.Stdout
-	os.Stdout = w
-	defer func() {
-		os.Stdout = stdout
-	}()
-
-	f()
-	w.Close()
-
-	var buf bytes.Buffer
-	io.Copy(&buf, r)
-	return buf.String()
-}
 
 func TestMarkdownTable(t *testing.T) {
 	matches := make([]*match.Match, 1)
@@ -38,10 +14,7 @@ func TestMarkdownTable(t *testing.T) {
 		Team2Name:  "England",
 		Team2Score: 12,
 	}
-	markdownString := captureOutput(func() {
-		OutputMarkdownTable(matches)
-	})
-	fmt.Println(markdownString)
+	markdownString := OutputMarkdownTable(matches)
 	if markdownString != `|     主队     | 比分  |  客队   |
 |--------------|-------|---------|
 | South Africa | 23-12 | England |
